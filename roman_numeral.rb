3f17b68f21d19c24d6b0c7c30225318d7c300229
nergdnvlt <tylerlundgren@yahoo.com>
2020-08-26 17:37:43 -0600

    Adding values store for lookup.

--- roman_numeral.rb
+++ roman_numeral.rb
@@ -1,4 +1,15 @@
 class RomanNumeral
+    attr_reader :values
+    
     def initialize()
+        @values = { 
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
     end
 end
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -1,4 +1,5 @@
 require './roman_numeral.rb'
+require 'pry'
 
 describe 'RomanNumeral' do
     describe 'basic validations and setup' do
@@ -6,6 +7,18 @@ describe 'RomanNumeral' do
             roman_numeral = RomanNumeral.new
 
             expect(roman_numeral).to be_a(RomanNumeral)
+        end
+
+        it 'contains a hash for value lookup' do
+            roman_numeral = RomanNumeral.new
+
+            expect(roman_numeral.values[:I]).to eq(1)
+            expect(roman_numeral.values[:V]).to eq(5)
+            expect(roman_numeral.values[:X]).to eq(10)
+            expect(roman_numeral.values[:L]).to eq(50)
+            expect(roman_numeral.values[:C]).to eq(100)
+            expect(roman_numeral.values[:D]).to eq(500)
+            expect(roman_numeral.values[:M]).to eq(1000)
         end
     end
 end
