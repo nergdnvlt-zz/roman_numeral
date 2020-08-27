7d2594b7a780d3ad946ef3c9f0ab6763d486198b
nergdnvlt <tylerlundgren@yahoo.com>
2020-08-27 07:59:47 -0600

    Deleting code to start a different approach.

--- roman_numeral.rb
+++ roman_numeral.rb
@@ -4,45 +4,9 @@ class RomanNumeral
     end
 
     def calc
-        @roman_num_arr.each_with_index do |value_1, index|       
-            if index < @roman_num_arr.length - 1
-                value_2 = @roman_num_arr[index + 1]
-                value_3 = @roman_num_arr[index + 2]
-                recursive_eval(value_1, value_2, value_3, index)
-            else
-               if @roman_num_arr[0] < @roman_num_arr[1]
-                 return @roman_num_arr[1] - @roman_num_arr[0]
-               else
-                 return @roman_num_arr.sum
-               end
-            end
-        end
-        if @roman_num_arr.length == 1
-            @roman_num_arr[0]
-        else
-            @roman_num_arr.sum
-        end
     end
 
     private
-
-    def recursive_eval(value_1, value_2, value_3, index)
-        if value_3 && value_1 > value_2 && value_2 < value_3
-            @roman_num_arr.delete_at(index)
-            @roman_num_arr.delete_at(index + 1)
-            @roman_num_arr[index] = value_1 + (value_3 - value_2)
-        else
-            if value_1 < value_2
-                replace = value_2 - value_1        
-                @roman_num_arr[index] = replace
-                @roman_num_arr.delete_at(index + 1)
-            else
-                replace = value_2 + value_1        
-                @roman_num_arr[index] = replace
-                @roman_num_arr.delete_at(index + 1)
-            end
-        end
-    end
     
     def initialize(roman_num_input)
         @values = { 
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -7,82 +7,11 @@ describe 'RomanNumeral' do
 
             expect(roman_numeral).to be_a(RomanNumeral)
         end
-
-        # REMOVED THE ATTR_READERS AS UNNECCESSARY
-        # it 'contains a hash for value lookup' do
-        #     roman_numeral = RomanNumeral.new('I')
-
-        #     expect(roman_numeral.values[:I]).to eq(1)
-        #     expect(roman_numeral.values[:V]).to eq(5)
-        #     expect(roman_numeral.values[:X]).to eq(10)
-        #     expect(roman_numeral.values[:L]).to eq(50)
-        #     expect(roman_numeral.values[:C]).to eq(100)
-        #     expect(roman_numeral.values[:D]).to eq(500)
-        #     expect(roman_numeral.values[:M]).to eq(1000)
-        # end
-
-        # it 'splits the roman numeral correctly' do
-        #     roman_numeral = RomanNumeral.new('II')
-
-        #     expect(roman_numeral.roman_num_arr).to eq([1, 1])
-        # end
-
-        # it 'splits a different roman numeral correctly' do
-        #     roman_numeral = RomanNumeral.new('MVCII')
-
-        #     expect(roman_numeral.roman_num_arr).to eq([1000, 5, 100, 1, 1])
-        # end
     end
 
-    describe 'beginning calculations with two digits' do
+    describe 'beginning calculations with a basic first array' do
         it 'when we start with something basic' do
-            expect(RomanNumeral.calc('IV')).to eq(4)
-        end
-
-        it 'when we alternate a simple subtraction' do
-            expect(RomanNumeral.calc('VX')).to eq(5)
-        end
-
-        it 'when we start with something basic that adds up' do
-            expect(RomanNumeral.calc('VI')).to eq(6)
-        end
-
-        it 'adds alternate set of two digits that add up' do
-            expect(RomanNumeral.calc('MD')).to eq(1500)
-        end
-
-        it 'adds additional alternate set of two digits that add up again' do
-            expect(RomanNumeral.calc('DL')).to eq(550)
-        end
-
-        it 'adds two of the same consecutive numbers fine' do
-            expect(RomanNumeral.calc('DD')).to eq(1000)
-        end
-
-        it 'adds alternate two of the same consecutive numbers fine' do
-            expect(RomanNumeral.calc('CC')).to eq(200)
-        end
-    end
-
-    describe 'start testing 3 digit calculations' do
-        it 'when we start with something basic subtraction' do
-            expect(RomanNumeral.calc('IIV')).to eq(3)
-        end
-
-        it 'when we start with another basic subtraction' do
-            expect(RomanNumeral.calc('IIC')).to eq(98)
-        end
-
-        it 'when we start with another basic subtraction' do
-            expect(RomanNumeral.calc('VVC')).to eq(90)
-        end
-
-        it 'when we start with a basic addition' do
-            expect(RomanNumeral.calc('CVV')).to eq(110)
-        end
-
-        it 'when we start with another basic addition' do
-            expect(RomanNumeral.calc('MDD')).to eq(2000)
+            expect(RomanNumeral.calc('MCM')).to eq(4)
         end
     end
 
