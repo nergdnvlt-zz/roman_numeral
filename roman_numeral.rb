2ff0da2f263c3e99c1504954f76531a81726c9ab
nergdnvlt <tylerlundgren@yahoo.com>
2020-08-30 15:53:40 -0600

    Error in the way that I'm finding duplicates. I am pulling all duplicates so ending up with multiples of the originals.

--- roman_numeral.rb
+++ roman_numeral.rb
@@ -4,6 +4,14 @@ class RomanNumeral
     end
 
     def calc
+        binding.pry        
+        if @romans[0].class == Integer && @romans.length == 2
+            eval_two_digit
+        else
+            @romans.map do |sub_array|
+                convert_negatives(sub_array)
+            end.sum
+        end
     end
 
     private
@@ -18,7 +26,56 @@ class RomanNumeral
             'D': 500,
             'M': 1000
         }
-        @roman_num_arr = convert(roman_num_input.chars)
+        @romans = convert(roman_num_input.chars)
+        binding.pry
+        # combine_arrays(seperate_arrays)
+    end
+
+    def eval_two_digit
+        return convert_negatives(@romans.reverse) if @romans[0] < @romans[1]
+    end
+
+    def combine_arrays(duplicate_array)
+        @romans.delete_if { |x| duplicate_array.flatten.include?(x) }
+        duplicate_array.each do |sub|
+            @romans << sub
+        end
+    end
+
+
+    def seperate_arrays
+        pull_first(find_dupes).zip(pull_last(find_dupes)).map do |selection|
+            @romans.slice(selection[0]...(selection[1] + 1))
+        end
+    end
+
+    def pull_first(duplicate_array)
+        duplicate_array.map do |dupe|
+            @romans.index(dupe)
+        end
+    end
+
+    def pull_last(duplicate_array)
+        duplicate_array.map do |dupe|
+            @romans.rindex(dupe)
+        end
+    end
+
+    def find_dupes
+        binding.pry
+        @romans.group_by{ |e| e }.select { |k, v| v.size > 1 }.map(&:first)
+    end
+
+    def convert_negatives(sub_group)
+        return sub_group if sub_group.class == Integer
+
+        sub_group.map do |num|
+            if num == sub_group[0]
+                num
+            else
+                num * -1
+            end
+        end.sum
     end
 
     def convert(initial_roman_numerals)
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -1,36 +1,50 @@
 require './roman_numeral.rb'
+require 'pry'
 
 describe 'RomanNumeral' do
     describe 'basic validations and setup' do
-        it 'creates the model fine' do
-            roman_numeral = RomanNumeral.new('I')
+        # it 'creates the model fine' do
+        #     roman_numeral = RomanNumeral.new('I')
 
-            expect(roman_numeral).to be_a(RomanNumeral)
-        end
+        #     expect(roman_numeral).to be_a(RomanNumeral)
+        # end
     end
 
     describe 'beginning calculations with a basic first array' do
-        it 'when we start with something basic' do
-            expect(RomanNumeral.calc('MCM')).to eq(4)
-        end
+        # it 'when we start with something basic' do
+        #     expect(RomanNumeral.calc('MCM')).to eq(1900)
+        # end
+
+        # it 'when we start with something basic' do
+        #     expect(RomanNumeral.calc('MCMII')).to eq(1902)
+        # end
+
+        # it 'when we start with something basic' do
+        #     expect(RomanNumeral.calc('MCMLXL')).to eq(1990)
+        # end
+
+
+        # it 'when we start with something basic' do
+        #     expect(RomanNumeral.calc('MCMLXLII')).to eq(1992)
+        # end
     end
 
     describe 'Example 1: test for 3' do
-        it 'when we start mixing it up' do
-            expect(RomanNumeral.calc('III')).to eq(3)
-        end
+        # it 'when we start mixing it up' do
+        #     expect(RomanNumeral.calc('III')).to eq(3)
+        # end
 
-        it 'Example 2: test for 4' do
-            expect(RomanNumeral.calc('IV')).to eq(4)
-        end
+        # it 'Example 2: test for 4' do
+        #     expect(RomanNumeral.calc('IV')).to eq(4)
+        # end
 
-        it 'Example 3: test for 9' do
-            expect(RomanNumeral.calc('IX')).to eq(9)
-        end
+        # it 'Example 3: test for 9' do
+        #     expect(RomanNumeral.calc('IX')).to eq(9)
+        # end
 
-        it 'Example 4: test for 58' do
-            expect(RomanNumeral.calc('LVIII')).to eq(58)
-        end
+        # it 'Example 4: test for 58' do
+        #     expect(RomanNumeral.calc('LVIII')).to eq(58)
+        # end
 
         it 'Example 5: test for 1994' do
             expect(RomanNumeral.calc('MCMXCIV')).to eq(1994)
